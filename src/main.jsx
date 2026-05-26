bd898759796c3c638ed2eccfd674d611dc1153b4
2Fabi1 <158741663+2Fabi1@users.noreply.github.com>
2026-05-26 11:13:02 +0200

    Import Homepage component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.jsx";
+import Homepage from "./Homepage.jsx";
 
 const root = document.getElementById("root");
 
