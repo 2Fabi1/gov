aa3c18df1c6ab37f95615d159360c55f6dad2298
K13 <K13@example.com>
2026-05-26 09:16:09 +0200

    fixed fetching url

--- Downloads/gov-master/gov-master/src/App.jsx
+++ Downloads/gov-master/gov-master/src/App.jsx
@@ -10,7 +10,7 @@ function App({ ids, sexLetter, type }) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch("http://localhost:3001/aggregate", {
+        const res = await fetch("./api/aggregate", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
